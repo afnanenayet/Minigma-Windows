c828829523a314be4343fd014a4443ac654d9b13
earmuffz21 <earmuffz21@gmail.com>
2014-03-25 19:22:05 -0400

    Create stdafx.h
    I created this in visual studio

---
+++ stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <limits>
+#include "globalvars.h"
+#include "decode.h"
+#include "encoder.h"
