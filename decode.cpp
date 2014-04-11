3ce5c4d69394224bd4e3853c7e66cd970e3c40bb
Afnan Enayet <earmuffz21@gmail.com>
2014-04-11 00:22:50 -0400

    cleaned out decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -1,23 +1,9 @@
-/*#include <iostream>
-#include <algorithm>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <string>
-#include <cmath>
-#include <limits>
-#include "globalvars.h" all of the prev headers (deprecated) are in stdafx.h */
+
 #include "decode.h"
 #include "stdafx.h"
 
 using namespace std;
 
-//global variables (now redundant, moved to globalvars.h)
-/*
-const int array_length = 83;
-char c_cipher[array_length] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', '[', ']', '{', '}', '<', '>', '?' }; //array to be shuffled based on user password
-char c_standard[array_length] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', '[', ']', '{', '}', '<', '>', '?' }; //array to be shuffled based on user password
-*/
 
 template <typename Iter>
 size_t index_of(Iter first, Iter last, typename const std::iterator_traits<Iter>::value_type& x)
