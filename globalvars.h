16ad7af8a0b72aabac224e8291bd5474c2e551d6
Afnan Enayet <earmuffz21@gmail.com>
2014-05-12 14:18:52 -0400

    Update globalvars.h
    Moved indexing template from decode/encode to create an efficient global method of indexing/replacing elements of an array

--- globalvars.h
+++ globalvars.h
@@ -2,4 +2,11 @@ namespace {
 	const int array_length = 83;
 	extern char c_standard[83] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', '[', ']', '{', '}', '<', '>', '?' }; //array to be shuffled based on user password
 	extern char c_cipher[array_length] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', '[', ']', '{', '}', '<', '>', '?' }; //array to be shuffled based on user password
+	size_t index_of(Iter first, Iter last, typename const std::iterator_traits<Iter>::value_type& x)
+	{
+		size_t i = 0;
+		while (first != last && *first != x)
+			++first, ++i;
+		return i;
+	} //get index of array
 }
