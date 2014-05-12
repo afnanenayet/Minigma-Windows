132ac96f1e0c681c210e0b784c8c1ab19fd38030
Afnan Enayet <earmuffz21@gmail.com>
2014-05-12 14:20:14 -0400

    cleanup decode.cpp
    removed indexing template

--- decode.cpp
+++ decode.cpp
@@ -1,24 +1,23 @@
-
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <limits>
+#include "globalvars.h"
 #include "decode.h"
-#include "stdafx.h"
 
 using namespace std;
 
+//global variables (now redundant, moved to globalvars.h)
 
-template <typename Iter>
-size_t index_of(Iter first, Iter last, typename const std::iterator_traits<Iter>::value_type& x)
-{
-	size_t i = 0;
-	while (first != last && *first != x)
-		++first, ++i;
-	return i;
-} //get index of array
-
-void decoder(std::string coded) { //better transcoder
+void decoder(std::string coded) {
 	int c_position;
 	for (unsigned int n = 0; n < coded.length(); n++) { //replace elements from standard array to decode
 		c_position = index_of(c_cipher, c_cipher + array_length, coded.at(n));
-		cout << c_standard[c_position - 1];
+		cout << c_standard[c_position];
 	}
 }
 
@@ -52,39 +51,24 @@ int decode_main() {
 			user_seed += c_password[i];
 			user_seed = user_seed * (i + 100);
 		}
-
-
-		//cout << "DEBUG: user_seed = " << user_seed << endl; //delete this before production
 		std::srand(user_seed);
-
 		std::random_shuffle(c_cipher, c_cipher + array_length);
-		/*	for (int n = 0; n < array_length; n++) {
-		std::cout << c_cipher[n];
-		}*/
 		std::cout << endl;
-
 		std::cout << "Type in your message to be decoded." << endl;
 		std::getline(cin, coded_message);
 		std::cout << endl;
 		decoder(coded_message);
-
-
 		c_password.erase(c_password.begin(), c_password.end()); //reset password jic user wants to decode again
-
-
-
 		std::cout << endl;
 		std::cout << "1: decode again" << endl;
 		std::cout << "2: return to menu" << endl;
 		//menu hierarchy
-
 		do
 		{
 			cin >> quitorno;
 			std::cout << endl;
-
 			//will go back to main menu depending on user input or will repeat
-			if (quitorno == 1) {
+			if (quitorno == 1) { 
 				user_quit = false;
 				cin.clear();
 				valid_input = true;
